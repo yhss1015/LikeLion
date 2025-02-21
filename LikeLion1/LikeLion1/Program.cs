f09c16b693088faef96260cb0d5e95d44a248d4c
yhss1015 <dudgns8481@gmail.com>
2025-02-21 09:57:11 +0900

    Study1

---
+++ LikeLion1/LikeLion1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //출력
+            Console.WriteLine("깃허브 연결연습");
+        }
+    }
+}
