8537e412d020ef87bee8b6e806a65cc7925e5bd5
yhss1015 <dudgns8481@gmail.com>
2025-02-21 10:22:54 +0900

    Study2

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수선언
+            int age;
+            age = 25;
+            Console.WriteLine(age);
+        }
+    }
+}
