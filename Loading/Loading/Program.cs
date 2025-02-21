3c2649fad0bae894ae3d549d404f98bf3a9a63e3
yhss1015 <dudgns8481@gmail.com>
2025-02-21 16:02:02 +0900

    Loading

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■■");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("텍스트 엘든링");
+
+            Console.WriteLine("스토리1");
+
+
+
+        }
+    }
+}
---
+++ ReadLine/ReadLine/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////사용자 입력을 문자열 받기
+            //Console.Write("이름을 입력하세요: ");
+            //string userName = Console.ReadLine(); //사용자롤 부터 입력 받기
+
+            //Console.WriteLine($"안녕하세요, {userName}님");
+
+            ////문자열을 정수로 반환
+            //Console.Write("나이를 입력하세요 :");
+            //string input = Console.ReadLine(); //사용자로부터 입력 받기
+            //int age = int.Parse(input);
+
+            //Console.WriteLine($"내년에는 {age + 1}살이 되겠군요!");// 변환된 값 사용
+            //Console.WriteLine("현재는 " + age + "살 이시군요!");
+            //Console.WriteLine("내년에는 {0} 살이 되겠군요", age + 1);
+
+            //Console.Write("루인 스킬 피해 입력 : ");
+            //float ruinSkill = float.Parse(Console.ReadLine());
+            //Console.Write("카드 게이지 획득량 입력 : ");
+            //float cardGauge = float.Parse(Console.ReadLine());
+            //Console.Write("각성기 피해 입력 : ");
+            //float ultiSkill = float.Parse(Console.ReadLine());
+            //Console.Write("최대 마나 입력 : ");
+            //float maxMana = float.Parse(Console.ReadLine());
+            //Console.Write("전투 중 마나 회복량 입력 : ");
+            //float battleMana = float.Parse(Console.ReadLine());
+            //Console.Write("비전투 중 마나 회복량 입력 : ");
+            //float nonBattleMana = float.Parse(Console.ReadLine());
+            //Console.Write("이동속도 입력 : ");
+            //float speed = float.Parse(Console.ReadLine());
+            //Console.Write("탈 것 속도 입력 : ");
+            //float vehicleSpeed = float.Parse(Console.ReadLine());
+            //Console.Write("운반 속도 입력 : ");
+            //float conveyanceSpeed = float.Parse(Console.ReadLine());
+            //Console.Write("스킬 재사용 대기시간 감소 입력 : ");
+            //float returnTime = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+            //Console.WriteLine("");
+            //Console.WriteLine("-----------활동-----------");
+
+            //Console.WriteLine($"루인 스킬 피해                 : {ruinSkill}%");
+            //Console.WriteLine($"카드 게이지 획득량             : {cardGauge}%");
+            //Console.WriteLine($"각성기 피해                    : {ultiSkill}%");
+            //Console.WriteLine($"최대 마나                      : {maxMana}");
+            //Console.WriteLine($"전투 중 마나 회복량            : {battleMana}");
+            //Console.WriteLine($"비전투 중 마나 회복량          : {nonBattleMana}");
+            //Console.WriteLine($"이동 속도                      : {speed}%");
+            //Console.WriteLine($"탈 것 속도                     : {vehicleSpeed}%");
+            //Console.WriteLine($"운반 속도                      : {conveyanceSpeed}%");
+            //Console.WriteLine($"스킬 재사용 대기시간 감소      : {returnTime}%");
+
+
+
+
+        }
+    }
+}
