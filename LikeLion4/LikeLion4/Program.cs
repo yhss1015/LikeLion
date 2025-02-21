733befc48ea6b2c0438ce2f97e47833d2dbb0982
yhss1015 <dudgns8481@gmail.com>
2025-02-21 14:34:52 +0900

    Study5

---
+++ LikeLion4/LikeLion4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////문자열
+            //string greeting;    //문자열 변수를 선언
+            //greeting = "Hello, World";  //변수에 값을 저장
+
+            ////변수의 값을 사용
+            //Console.WriteLine(greeting);//출력 : Hello, World!
+
+            //// 변수 선언과 초기화를 한번에 수행
+            //int score = 100;    // 정수형 변수 선언&100 초기화
+            //double temp = 36.6; // 실수형 변수 선언&36.6 초기화
+            //string city = "Seoul";  // 문자열 변수 선언&초기화
+
+            ////변수 출력
+            //Console.WriteLine(score); // 출력 100
+            //Console.WriteLine(temp); // 출력 36.6
+            //Console.WriteLine(city); // 출력 Seoul
+
+            //// 같은 데이터 타입의 변수를 쉼표 구분하여 선언
+            //int x = 10,y = 20,z = 30;   //정수형 변수 x,y,z 선언&초기화
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+
+            //const double Pi = 3.14159; // 상수 pi선언 및 초기화
+            //const int MaxScore = 100;  // 상수 MaxScore 선언 및 초기화
+
+            //// 출력
+            //Console.WriteLine("Pi : "+Pi); // 출력 Pi : 3.14159
+            //Console.WriteLine("MaxScore : " + MaxScore); // 출력 MaxScore : 100
+
+
+            int attack = 16755;
+            int maxHp = 78103;
+            int critical = 36;
+            int specialization = 1017;
+            int subdue = 41;
+            int speed = 611;
+            int patience = 22;
+            int proficieny = 39;
+
+            Console.WriteLine("기본 특성");
+            Console.WriteLine("공격력 : " + attack);
+            Console.WriteLine("최대 생명력 : "+maxHp);
+            Console.WriteLine();
+            Console.WriteLine("전투 특성");
+            Console.WriteLine("치명 : " + critical);
+            Console.WriteLine("특화 : " + specialization);
+            Console.WriteLine("제압 : " + subdue);
+            Console.WriteLine("신속 : " + speed);
+            Console.WriteLine("인내 : " + patience);
+            Console.WriteLine("숙련 : " + proficieny);
+
+
+
+        }
+    }
+}
---
+++ LikeLion5/LikeLion5/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// 숫자 데이터 형식 : 정수와 실수를 다룰 때 사용하는 다양한 타입
+            //int integerNum = 10;    //정수 데이터
+            //float floatNum = 3.14f; //단정밀도 실수
+            //double doubleNum = 3.14159; //배정밀도 실수
+
+            //Console.WriteLine(integerNum);
+            //Console.WriteLine(floatNum);
+            //Console.WriteLine(doubleNum);
+
+            //float singlePrecision = 3.14f;  //단정밀도 실수
+            //double doublePrecision = 3.1434234; //배정밀도 실수(8바이트)
+            //decimal highPrecision = 3.145353422255m; //고정밀도 실수(16바이트)
+
+            //Console.WriteLine(singlePrecision);
+            //Console.WriteLine(doublePrecision);
+            //Console.WriteLine(highPrecision);
+
+            ////char형식 : 단일 문자를 표현
+            //char letter = 'A'; // 문자 'A' 저장
+            //char symbol = '#'; // 특수 기호 저장
+            //char number = '9'; // 숫자 형태의 문자 저장
+
+            //Console.WriteLine(letter);
+            //Console.WriteLine(symbol);
+            //Console.WriteLine(number);
+
+            ////string 형식 : 여러 문자를 저장
+            //string greeting = "Hello World";
+            //string name = "Alice";
+
+            //Console.WriteLine(greeting);
+            //Console.WriteLine(name);
+
+            ////Hello World Alice
+            //Console.WriteLine(greeting + " "+ name);
+
+            ////bool형식 : 참(true) ==1 거짓(false) ==0
+
+            //bool isRunning = true;  //프로그램 실행 상태
+            //bool isFininshed = false; //프로그램 종료 상태
+
+            //Console.WriteLine(isRunning);
+            //Console.WriteLine(isFininshed);
+
+            ////닷넷 형식: 기본형식의 닷넷표현
+            //Int32 number = 123; //int닷넷
+            //String text = "Hello"; //string 닷넷
+            //Boolean flag = true; //bool의 닷넷
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(text);
+            //Console.WriteLine(flag);
+
+            ////int 래퍼 형식의 메서드 활용
+            //int number = 123;
+
+            //string numberAsString = number.ToString(); // 정수를 문자열로
+
+            ////bool 래퍼형식
+            //bool flag = true;
+
+            //string flagAsString = flag.ToString(); // 논리값을 문자열로
+
+            //Console.WriteLine(numberAsString);
+            //Console.WriteLine(flagAsString);
+
+
+            
+
+
+
+        }
+    }
+}
