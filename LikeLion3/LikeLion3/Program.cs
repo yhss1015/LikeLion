dcf38219971b0c29c52bb122cd184751e915e405
yhss1015 <dudgns8481@gmail.com>
2025-02-21 10:42:29 +0900

    Study3

---
+++ LikeLion3/LikeLion3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //캐릭터
+            //hp : 100
+            //att : 56.7
+            //캐릭터이름 : ??
+            //등급 : S
+
+            int hp = 100;
+            float att = 56.7f;
+            string name = "LikeLion";
+            char grade = 'S';
+
+            Console.WriteLine(hp);
+            Console.WriteLine(att);
+            Console.WriteLine(name);
+            Console.WriteLine(grade);
+            
+        }
+    }
+}
