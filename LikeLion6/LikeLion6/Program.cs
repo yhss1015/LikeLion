3506fa7cb52baade0c9585fab2d7af394b9be707
yhss1015 <dudgns8481@gmail.com>
2025-02-24 10:32:53 +0900

    Study6

---
+++ LikeLion6/LikeLion6/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////이진수를 정수로 변환
+            //console.write("2진수를 입력하세요:");
+            //string binaryinput = console.readline(); // 문자열로 입력
+            //int decimalvalue = convert.toint32(binaryinput,2); //2진수 -> 10진수 변환
+
+            ////정수를 이진수로 변환
+            //string binaryoutput = convert.tostring(decimalvalue,2); //10진수 -> 2진수
+
+            //console.writeline($"입력한 이진수 : {binaryinput}");
+            //console.writeline($"10진수로 변환 : {decimalvalue}");
+            //console.writeline($"다시 이진수로 변환 : {binaryoutput}");
+            //컨트롤 + k + c
+            //컨트롤 + k + u
+            //컨트롤 쉬프트 + /
+
+            ////var를 사용하여 변수 선언
+            //var name = "Alice"; //문자열로 추론
+            //var age = 25; //정수로 추론
+            //var isStudent = true; //논리값으로 추론
+
+            //Console.WriteLine($"이름 : {name}\n나이 : {age}\n학생 : {isStudent}");
+
+            ////default 키워드 사용한 기본값 설정
+            //int defaultInt = default; //기본값 : 0
+            //string defaultString = default; //기본값 : null
+            //bool defaultBool = default; //기본값 : false
+
+            //Console.WriteLine($"정수 기본값 : {defaultInt}\n문자열 기본값 : {defaultString}\n논리값 기본값 : {defaultBool}");
+
+            int sum = 0;
+            //int a = 5, b = 3;
+
+            //int minus = 0;
+
+            //sum = a + b; //산술 연산자 사용
+            //minus = a - b;
+            //Console.WriteLine($"a({a})+b({b}) : {sum}");
+            //Console.WriteLine($"a({a})-b({b}) : {minus}");
+
+
+
+            //int c = 0, d = 0;
+
+            //c = 10;
+            //d = 3;
+            //sum = c % d;
+            //Console.WriteLine(sum);
+
+            //int number = 7;
+            //sum = number % 2; // 0이나오면 짝수 1 홀수 
+
+            //Console.WriteLine("짝수 홀수 판별 : " + sum);
+
+            //bool isEqual = false;   // 거짓 0
+            //int a = 5;
+            //int b = 3;
+            //int c = 5;
+
+            //Console.WriteLine($"a({a})==b({b}) : {a==b} a({a})==c({c}){a==c}");
+
+
+
+
+
+        }
+    }
+}
